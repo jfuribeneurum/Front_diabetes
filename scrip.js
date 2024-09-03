868c30fb796f0da1e66d2ddc3ac1e4b5d287f1a4
juan.uribe <desarrolladorfs1@neurum.com.co>
2024-09-03 10:15:12 -0500

    get sintomas sensoriales ok

--- scrip.js
+++ scrip.js
@@ -184,7 +184,7 @@ async function buscarPaciente() {
 
             const classificationField = document.querySelector('#classification');
             if (classificationField) {
-                classificationField.value = data.clasificacion|| "";
+                classificationField.value = data.clasificacion || "";
             }
 
             const trrField = document.querySelector('#trr');
@@ -196,11 +196,272 @@ async function buscarPaciente() {
                         break;
                     }
                 }
-           }
+            }
+
+            toggleNephropathySection();
+
+            // Datos de síntomas sensoriales
+            const sensorySymptomsField = document.querySelector('#sensorySymptoms');
+            if (sensorySymptomsField) {
+                sensorySymptomsField.value = data.tiene_evaluacion_neurologica || "";
+            } else {
+                console.error('Elemento con id "sensorySymptoms" no encontrado.');
+            }
+
+            const fechaConsultaField = document.querySelector('#consultationDate');
+            if (fechaConsultaField) {
+                fechaConsultaField.value = data.fecha_consulta ? formatDate(data.fecha_consulta) : "";
+            } else {
+                console.error('Elemento con id "consultationDate" no encontrado.');
+            }
 
 
 
-            
+            const localizacionField = document.querySelector('#localizacion');
+            if (localizacionField) {
+                const opciones = localizacionField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.localizacion.toLowerCase()) {
+                        localizacionField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "location" no encontrado.');
+            }
+
+            const intensidadField = document.querySelector('#intensidad');
+            if (intensidadField) {
+                const opciones = intensidadField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].value === data.intensidad) {
+                        intensidadField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "intensity" no encontrado.');
+            }
+
+            const compromisoField = document.querySelector('#compromiso_dolor');
+            if (compromisoField) {
+                const opciones = compromisoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.compromiso_dolor.toLowerCase()) {
+                        compromisoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "compromiso_dolor" no encontrado.');
+            }
+
+            const lateralidadField = document.querySelector('#lateralidad_dolor');
+            if (lateralidadField) {
+                const opciones = lateralidadField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.lateralidad_dolor.toLowerCase()) {
+                        lateralidadField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "lateralidad_dolor" no encontrado.');
+            }
+
+            const presentaDolorField = document.querySelector('#presentaDolor');
+            if (presentaDolorField) {
+                presentaDolorField.value = data.presenta_dolor || "";
+            } else {
+                console.error('Elemento con id "presentaDolor" no encontrado.');
+            }
+
+            //seccion hormigueo
+            const presentaHormigueoField = document.querySelector('#tingling');
+            if (presentaHormigueoField) {
+                presentaHormigueoField.value = data.presenta_hormigueo || "";
+            } else {
+                console.error('Elemento con id "presentaHormigueo" no encontrado.');
+            }
+
+
+            const localizacionhormigueoField = document.querySelector('#localizacionhormigueo');
+            if (localizacionhormigueoField) {
+                const opciones = localizacionhormigueoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.localizacion_hormigueo.toLowerCase()) {
+                        localizacionhormigueoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "localizacionhormigueoField" no encontrado.');
+            }
+
+            const intensidadhormigueoField = document.querySelector('#intensidadhormigueo');
+            if (intensidadhormigueoField) {
+                const opciones = intensidadhormigueoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].value === data.intensidad_hormigueo) {
+                        intensidadhormigueoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "intensityhormigueo" no encontrado.');
+            }
+
+            const compromisohormigueoField = document.querySelector('#compromisohormigueo');
+            if (compromisohormigueoField) {
+                const opciones = compromisohormigueoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.compromiso_hormigueo.toLowerCase()) {
+                        compromisohormigueoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "compromiso_dolor" no encontrado.');
+            }
+
+            const lateralidadhormigueoField = document.querySelector('#lateralidadhormigueo');
+            if (lateralidadhormigueoField) {
+                const opciones = lateralidadhormigueoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.lateralidad_hormigueo.toLowerCase()) {
+                        lateralidadhormigueoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "lateralidad_dolor" no encontrado.');
+            }
+
+
+            // Sección entumecimiento
+            const presentaEntumecimientoField = document.querySelector('#entumecimiento');
+            if (presentaEntumecimientoField) {
+                presentaEntumecimientoField.value = data.presenta_entumecimiento || "";
+            } else {
+                console.error('Elemento con id "entumecimiento" no encontrado.');
+            }
+
+            const localizacionEntumecimientoField = document.querySelector('#localizacionentumecimiento');
+            if (localizacionEntumecimientoField) {
+                const opciones = localizacionEntumecimientoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.localizacion_entumecimiento.toLowerCase()) {
+                        localizacionEntumecimientoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "localizacionentumecimiento" no encontrado.');
+            }
+
+            const intensidadEntumecimientoField = document.querySelector('#intensidadentumecimiento');
+            if (intensidadEntumecimientoField) {
+                const opciones = intensidadEntumecimientoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].value === data.intensidad_entumecimiento) {
+                        intensidadEntumecimientoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "intensidadentumecimiento" no encontrado.');
+            }
+
+            const compromisoEntumecimientoField = document.querySelector('#compromisoentumecimiento');
+            if (compromisoEntumecimientoField) {
+                const opciones = compromisoEntumecimientoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.compromiso_entumecimiento.toLowerCase()) {
+                        compromisoEntumecimientoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "compromisoentumecimiento" no encontrado.');
+            }
+
+            const lateralidadEntumecimientoField = document.querySelector('#lateralidadentumecimiento');
+            if (lateralidadEntumecimientoField) {
+                const opciones = lateralidadEntumecimientoField.options;
+                for (let i = 0; i < opciones.length; i++) {
+                    if (opciones[i].text.toLowerCase() === data.lateralidad_entumecimiento.toLowerCase()) {
+                        lateralidadEntumecimientoField.value = opciones[i].value;
+                        break;
+                    }
+                }
+            } else {
+                console.error('Elemento con id "lateralidadentumecimiento" no encontrado.');
+            }
+
+
+            // Sección Pérdida de Sensibilidad
+const presentaSensibilidadField = document.querySelector('#sensitivityLoss');
+if (presentaSensibilidadField) {
+    presentaSensibilidadField.value = data.presenta_perdida_sensibilidad || "";
+} else {
+    console.error('Elemento con id "sensitivityLoss" no encontrado.');
+}
+
+const localizacionSensibilidadField = document.querySelector('#sensitivityLossLocation');
+if (localizacionSensibilidadField) {
+    const opciones = localizacionSensibilidadField.options;
+    for (let i = 0; i < opciones.length; i++) {
+        if (opciones[i].value === data.localizacion_perdida_sensibilidad) {
+            localizacionSensibilidadField.value = opciones[i].value;
+            break;
+        }
+    }
+} else {
+    console.error('Elemento con id "sensitivityLossLocation" no encontrado.');
+}
+
+const intensidadSensibilidadField = document.querySelector('#sensitivityLossIntensity');
+if (intensidadSensibilidadField) {
+    const opciones = intensidadSensibilidadField.options;
+    for (let i = 0; i < opciones.length; i++) {
+        if (opciones[i].value === data.intensidad_perdida_sensibilidad) {
+            intensidadSensibilidadField.value = opciones[i].value;
+            break;
+        }
+    }
+} else {
+    console.error('Elemento con id "sensitivityLossIntensity" no encontrado.');
+}
+
+const compromisoSensibilidadField = document.querySelector('#sensitivityLossLocationType');
+if (compromisoSensibilidadField) {
+    const opciones = compromisoSensibilidadField.options;
+    for (let i = 0; i < opciones.length; i++) {
+        if (opciones[i].text.toLowerCase() === data.compromiso_perdida_sensibilidad.toLowerCase()) {
+            compromisoSensibilidadField.value = opciones[i].value;
+            break;
+        }
+    }
+} else {
+    console.error('Elemento con id "sensitivityLossLocationType" no encontrado.');
+}
+
+const lateralidadSensibilidadField = document.querySelector('#sensitivityLossSymmetry');
+if (lateralidadSensibilidadField) {
+    const opciones = lateralidadSensibilidadField.options;
+    for (let i = 0; i < opciones.length; i++) {
+        if (opciones[i].value === data.lateralidad_perdida_sensibilidad) {
+            lateralidadSensibilidadField.value = opciones[i].value;
+            break;
+        }
+    }
+} else {
+    console.error('Elemento con id "sensitivityLossSymmetry" no encontrado.');
+}
+
+
+
+
 
             // Calcular y mostrar la edad
             if (data.fecha_nacimiento) {
@@ -339,16 +600,41 @@ async function actualizarPaciente() {
         data.clasificacion = classificationField.value || "";
     }
 
-    //const trrField = document.querySelector('#trr');
-    //if (trrField) {
-        //const opciones = trrField.options;
-        //for (let i = 0; i < opciones.length; i++) {
-            //if (opciones[i].text.toLowerCase() === data.trr.toLowerCase()) {
-               // data.trr = opciones[i].value;
-                //break;
-            //}
-       // }
-    //}
+
+    // Manejo de los datos de síntomas sensoriales
+    data.tiene_evaluacion_neurologica = document.querySelector('#sensorySymptoms').value || "";
+
+
+    // Localización del dolor
+    const localizacionField = document.querySelector('#localizacion');
+    if (localizacionField) {
+        data.localizacion = localizacionField.value || "";
+    }
+
+    // Intensidad del dolor
+    const intensidadField = document.querySelector('#intensidad');
+    if (intensidadField) {
+        data.intensidad = intensidadField.value || "";
+    }
+
+    // Compromiso del dolor
+    const compromisoField = document.querySelector('#compromiso_dolor');
+    if (compromisoField) {
+        data.compromiso_dolor = compromisoField.value || "";
+    }
+
+    // Lateralidad del dolor
+    const lateralidadField = document.querySelector('#lateralidad_dolor');
+    if (lateralidadField) {
+        data.lateralidad_dolor = lateralidadField.value || "";
+    }
+
+    // Presencia de dolor
+    const presentaDolorField = document.querySelector('#presentaDolor');
+    if (presentaDolorField) {
+        data.presenta_dolor = presentaDolorField.value || "";
+    }
+
 
     try {
         const response = await fetch(`${apiUrl}/${numeroDocumento}`, {
@@ -438,6 +724,32 @@ function toggleRetinopathySection() {
     }
 }
 
+// Mostrar u ocultar la sección según el valor del <select> diabeticNephropathy
+function toggleDolorSection() {
+    const selectElement = document.querySelector('#diabeticNephropathy');
+    const toggleDolorSection = document.querySelector('#nephropathySection');
+    if (selectElement && toggleDolorSection) {
+        if (selectElement.value === 'si') {
+            toggleDolorSection.classList.remove('hidden');
+        } else {
+            toggleDolorSection.classList.add('hidden');
+        }
+    }
+}
+
+// Mostrar u ocultar la sección según el valor del <select> diabeticNephropathy
+function toggleNephropathySection() {
+    const selectElement = document.querySelector('#diabeticNephropathy');
+    const nephropathySection = document.querySelector('#nephropathySection');
+    if (selectElement && nephropathySection) {
+        if (selectElement.value === 'si') {
+            nephropathySection.classList.remove('hidden');
+        } else {
+            nephropathySection.classList.add('hidden');
+        }
+    }
+}
+
 // Inicialización de eventos cuando el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("buscar-btn").addEventListener("click", buscarPaciente);
@@ -453,4 +765,19 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleRetinopathySection(); // Inicializar la visibilidad de la sección
         eyeExamSelect.addEventListener('change', toggleRetinopathySection);
     }
+
+    // Inicializar la visibilidad de la sección de nefropatía
+    const nephropathySelect = document.getElementById("diabeticNephropathy");
+    if (nephropathySelect) {
+        toggleNephropathySection(); // Inicializar la visibilidad de la sección
+        nephropathySelect.addEventListener('change', toggleNephropathySection);
+    }
 });
+
+
+// Inicializar la visibilidad de la sección de síntomas sensoriales
+const sensorySymptomsSelect = document.getElementById("sensorySymptoms");
+if (sensorySymptomsSelect) {
+    toggleSensorySymptomsSection(); // Inicializar la visibilidad de la sección
+    sensorySymptomsSelect.addEventListener('change', toggleSensorySymptomsSection);
+}
